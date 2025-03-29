0bb29eeec5a32785f381c960b4bdc4108f8bc772
HaroonKasor <mprgame02@gmail.com>
2025-03-29 16:59:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import requests
 import os
-import hashlib
 from datetime import datetime
 
 LINE_TOKEN = os.getenv("LINE_TOKEN")
 USGS_API = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson'
 EVENT_LOG = "notified_ids.txt"
 
-
 def send_line_notify(message):
     url = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {LINE_TOKEN}'}
     data = {'message': message}
     requests.post(url, headers=headers, data=data)
-
 
 def is_in_target_region(lat, lon, place):
     southeast_asia_keywords = [
@@ -27,12 +24,6 @@ def is_in_target_region(lat, lon, place):
         90.0 <= lon <= 110.0
     )
     return keyword_match or lat_lon_match
-
-
-def generate_event_hash(mag, place, time):
-    key = f"{round(mag, 1)}|{place}|{time}"
-    return hashlib.sha256(key.encode()).hexdigest()
-
 
 # โหลด event_id ที่เคยแจ้งเตือน
 if os.path.exists(EVENT_LOG):
@@ -50,15 +41,13 @@ try:
     for event in features:
         props = event["properties"]
         geo = event["geometry"]
+        event_id = event.get("id")  # ใช้ event['id'] ตรงจาก USGS
         mag = props.get("mag", 0)
         place = props.get("place", "")
-        time = props.get("time", 0)
         coords = geo.get("coordinates", [None, None])
         lon, lat = coords[0], coords[1]
 
-        event_id = generate_event_hash(mag, place, time)
-
-        if event_id not in notified_ids and mag >= 5.0 and is_in_target_region(lat, lon, place):
+        if event_id and event_id not in notified_ids and mag >= 5.0 and is_in_target_region(lat, lon, place):
             message = f"🌍 แผ่นดินไหว {mag} บริเวณ {place}"
             send_line_notify(message)
             print("✅ แจ้งเตือน:", message)
